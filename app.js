ca091f165f6a7f1717ef5582692b7ed23427ec00
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-18 11:50:42 +0530

    Initialize customer list and log reversed array

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const customerList = [];
+customerList.push(10);
+customerList.push(20);
+customerList.push(30);
+
+let revAr = customerList.reverse();
+
+
+console.log(revAr);
