1fd51585b1c233ce1a65aa600f4d3aeb088342ec
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-18 12:15:16 +0530

    Refactor product filtering logic and clean up commented code

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const customerList = [];
-customerList.push(10);
-customerList.push(20);
-customerList.push(30);
+//Array methods
 
-let revAr = customerList.reverse();
+// const customerList = [];
+// customerList.push(10);
+// customerList.push(20);
+// customerList.push(30);
+
+// let revAr = customerList.reverse();
 
 
-console.log(revAr);
+// console.log(revAr);
+
+//------       ----------------
+
+//let inStockItems = products.filter(product => product.inStock == false);
+
+let inStockItems = products.filter(
+    function (product) {
+        return productFilter(product);
+
+    }
+)
+
+function productFilter(product) {
+    return product.inStock == true;
+}
+
+console.log(inStockItems);
+
