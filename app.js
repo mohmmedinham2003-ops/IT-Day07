c1554f07afb83222e767542fc06ac8c63a3d8cc1
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-24 22:24:11 +0530

    Update product fetching logic and enhance HTML structure for product display

--- app.js
+++ app.js
@@ -184,24 +184,38 @@
 // })
 
 
-fetch("https://jsonplaceholder.typicode.com/todos").then(res=>res.json()).then(data=>{
+fetch("https://fakestoreapi.com/products/1").then(res=>res.json()).then(data=>{
     console.log(data);
 
-    let tblTodoList = document.getElementById("tblTodoList");
-
+    let productContainer = document.getElementById("productContainer");
     let body=""
 
     data.forEach(element => {
         body+= `
-                <tr>
-            <td>${element.id}</td>
-            <td>${element.title}</td>
-            <td>${element.userId}</td>
-            <td>${element.completed}</td>
-        </tr>
+                <div class="col">
+                    <div class="card shadow-sm"> <svg aria-label="Placeholder: Thumbnail"
+                            class="bd-placeholder-img card-img-top" height="225" preserveAspectRatio="xMidYMid slice"
+                            role="img" width="100%" xmlns="http://www.w3.org/2000/svg">
+                            <title>Placeholder</title>
+                            <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
+                                dy=".3em">Thumbnail</text>
+                        </svg>
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group"> <button type="button"
+                                        class="btn btn-sm btn-outline-secondary">View</button> <button type="button"
+                                        class="btn btn-sm btn-outline-secondary">Edit</button> </div> <small
+                                    class="text-body-secondary">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
         `
     });
 
-    tblTodoList.innerHTML=body;
+    productContainer.innerHTML=body;
 
 })
