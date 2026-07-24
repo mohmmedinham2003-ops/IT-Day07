2e0301d758a6d9bd6b86b516e2183632fdd5f2aa
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-24 21:54:29 +0530

    Uncomment fetch logic for fetching todos from JSONPlaceholder and update table rendering

--- app.js
+++ app.js
@@ -161,6 +161,29 @@
 // Json
 
 
+// fetch("https://jsonplaceholder.typicode.com/todos").then(res=>res.json()).then(data=>{
+//     console.log(data);
+
+//     let tblTodoList = document.getElementById("tblTodoList");
+
+//     let body=""
+
+//     data.forEach(element => {
+//         body+= `
+//                 <tr>
+//             <td>${element.id}</td>
+//             <td>${element.title}</td>
+//             <td>${element.userId}</td>
+//             <td>${element.completed}</td>
+//         </tr>
+//         `
+//     });
+
+//     tblTodoList.innerHTML=body;
+
+// })
+
+
 fetch("https://jsonplaceholder.typicode.com/todos").then(res=>res.json()).then(data=>{
     console.log(data);
 
